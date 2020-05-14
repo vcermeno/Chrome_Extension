c04f5c9907f9e3bd725338f6b2a1d33066bc7b0e
Vivian Cermeno <viviancermeno@vivians-mbp.attlocal.net>
2020-05-14 13:11:16 -0700

    It's working!!!!! History finally deletes

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-//
-//You can remove browsing data
-//https://developer.chrome.com/extensions/browsingData
-//
+function browsingdata(){
+  chrome.browsingData.remove({
+    "originTypes": {
+      "protectedWeb": true, // Set to true or true as per your requirement
+      "unprotectedWeb":true,// Set to true or true as per your requirement
+      "extension":true    // Set to true or true as per your requirement
+    }
+  }, {
+    "appcache": true, // Set to true or true as per your requirement
+    "cache": true, // Set to true or true as per your requirement
+    "cookies": true, // Set to true or true as per your requirement
+    "downloads": true, // Set to true or true as per your requirement
+    "fileSystems": true, // Set to true or true as per your requirement
+    "formData": true, // Set to true or true as per your requirement
+    "history": true, // Set to true or true as per your requirement
+    "indexedDB": true, // Set to true or true as per your requirement
+    "localStorage": true, // Set to true or true as per your requirement
+    "pluginData": true, // Set to true or true as per your requirement
+    "passwords": false, // Set to true or true as per your requirement
+    "webSQL": true // Set to true or true as per your requirement
+  }, function (){
+      console.log("All data is Deleted...");
+  });
+}
+window.onload = browsingdata;
