a1bad21f268ba79c59c276eb9b6b9b71a15c40f2
Vivian Cermeno <viviancermeno@vivians-mbp.attlocal.net>
2020-05-14 15:17:41 -0700

    Deletes browser history from 5 minutes before

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+const millisecondsPerTenMin = 1000 * 60 * 5;
+let tenMinAgo = (new Date()).getTime() - millisecondsPerTenMin; //Time for all time - 1 minute
+
+//when you double click allow access to index.html page
+//have user copy paste url(s) and sumbit
+// on submit, save url(s) in chrome storage
+//when user clicks on icon then retrieve url(s) from chrome storage
+//and insert url(s) into newPage function
+//Extension: opens multiple tabs
+
 function browsingdata(){
   chrome.browsingData.remove({
+    "since": tenMinAgo,
     "originTypes": {
       "protectedWeb": true, // Set to true or true as per your requirement
       "unprotectedWeb":true,// Set to true or true as per your requirement
@@ -14,7 +25,7 @@ function browsingdata(){
     "formData": true, // Set to true or true as per your requirement
     "history": true, // Set to true or true as per your requirement
     "indexedDB": true, // Set to true or true as per your requirement
-    "localStorage": true, // Set to true or true as per your requirement
+    "localStorage": false, // Set to true or true as per your requirement
     "pluginData": true, // Set to true or true as per your requirement
     "passwords": false, // Set to true or true as per your requirement
     "webSQL": true // Set to true or true as per your requirement
@@ -22,4 +33,13 @@ function browsingdata(){
       console.log("All data is Deleted...");
   });
 }
-window.onload = browsingdata;
+
+function newPage(){
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.update(tabs[0].id, {url: "https://slack.com/"});
+});
+};
+
+chrome.browserAction.onClicked.addListener(browsingdata());
+chrome.browserAction.onClicked.addListener(newPage);
+
